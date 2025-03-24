c3ea8d7fcd766123829c44e172a928ebaf6e98d7
Michael Santos <msantana@swisscom.com>
2025-03-24 14:52:14 +0000

    added leverage 5 to strategy, must be configurable

--- AnandaStrategy.py
+++ AnandaStrategy.py
@@ -70,6 +70,23 @@ class AnandaStrategy(IStrategy):
     def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         return dataframe
     
+    def leverage(self, pair: str, current_time: datetime, current_rate: float,
+                 proposed_leverage: float, max_leverage: float, entry_tag: str | None, side: str,
+                 **kwargs) -> float:
+        """
+        Customize leverage for each new trade. This method is only called in futures mode.
+
+        :param pair: Pair that's currently analyzed
+        :param current_time: datetime object, containing the current datetime
+        :param current_rate: Rate, calculated based on pricing settings in exit_pricing.
+        :param proposed_leverage: A leverage proposed by the bot.
+        :param max_leverage: Max leverage allowed on this pair
+        :param entry_tag: Optional entry_tag (buy_tag) if provided with the buy signal.
+        :param side: "long" or "short" - indicating the direction of the proposed trade
+        :return: A leverage amount, which is between 1.0 and max_leverage.
+        """
+        return 5 # Should be configurable
+    
 # if __name__ == "__main__":
 #     parser = argparse.ArgumentParser(description="Binance Futures Position Manager")
 #     parser.add_argument("--apikey", required=True, help="Your Binance API Key")
