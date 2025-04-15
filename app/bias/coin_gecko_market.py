420d1e07e77d2322b0cf4766e866817e72191767
moonblade <moonblade168@gmail.com>
2025-04-16 01:12:54 +0530

    Add slugs in more places

--- app/bias/coin_gecko_market.py
+++ app/bias/coin_gecko_market.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from bias.coin_gecko_btc import COIN_GECKO_SYMBOLS
 from utils import get_logger
 from bias.interface import BiasInterface, BiasRequest, BiasResponse, BiasType
 
@@ -7,11 +8,12 @@ logger = get_logger()
 
 class CoinGeckoMarket(BiasInterface):
     paid = True
-    def get_crypto_market_data(self):
+    def get_crypto_market_data(self, biasRequest: BiasRequest):
         api_key = os.getenv("COINGECKO_API_KEY", "")
         url = f"https://pro-api.coingecko.com/api/v3/coins/markets?x_cg_pro_api_key={api_key}"
         params = {
             'vs_currency': 'usd',
+            # "ids": self.getidfromsymbol(biasRequest.symbol),
             'order': 'market_cap_desc',
             'per_page': 10,
             'page': 1,
@@ -33,14 +35,23 @@ class CoinGeckoMarket(BiasInterface):
                     total_negative += 1
         
         if total_positive > total_negative:
-            return BiasType.LONG
+            return BiasType.LONG, f"positive: {total_positive}, negative: {total_negative}"
         elif total_positive < total_negative:
-            return BiasType.SHORT
+            return BiasType.SHORT, f"positive: {total_positive}, negative: {total_negative}"
         else:
-            return BiasType.NEUTRAL
+            return BiasType.NEUTRAL, f"positive: {total_positive}, negative: {total_negative}"
 
 
     def bias(self, biasRequest: BiasRequest) -> BiasResponse:
-        market_data = self.get_crypto_market_data()
-        bias = self.calculate_trend_bias(market_data)
-        return BiasResponse(bias=bias)
+        market_data = self.get_crypto_market_data(biasRequest)
+        bias, reason = self.calculate_trend_bias(market_data)
+        return BiasResponse(bias=bias, usedSymbol=False, reason=reason)
+
+    def getidfromsymbol(self, symbol: str) -> str:
+        # Placeholder for the actual implementation to get the ID from the symbol
+        # This should be replaced with the actual logic to map symbols to IDs
+        for coin in COIN_GECKO_SYMBOLS:
+            if coin['symbol'].lower() == symbol.lower():
+                return coin['id']
+        logger.warning(f"Symbol {symbol} not found in COIN_GECKO_SYMBOLS.")
+        return "bitcoin"
